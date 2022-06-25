692d9b0a9f4f3de33da7172b1d3056bf1833ff3f
Deokhaeng <101077014+Deokhaeng@users.noreply.github.com>
2022-06-25 19:15:30 +0900

    CHORE: 최소 화면 폭 1200px 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import { Header, Section } from "./components";
+
 function App() {
-  return <div>트리플 과제</div>;
+  return (
+    <>
+      <AppWrap>
+        <Header />
+        <Section />
+      </AppWrap>
+    </>
+  );
 }
+
+const AppWrap = styled.div`
+  min-width: 1200px;
+`;
 
 export default App;
